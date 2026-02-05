2de13cf405ab7a9dc4178e6c9ccaa766649d032d
259833 <utkarshchaubey231@gmail.com>
2026-02-05 23:58:41 +0530

    clear the mess

--- React_1st/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Hello from'./Hello.jsx'
-function App() {
-  return (
-    <Hello/>
-  )
-}
-export default App
--- React_1st/src/Hello.jsx
+++
@@ -1,9 +0,0 @@
-function Hello() {
-    return(
-        <div>
-            <h1>Heres about me </h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia a maxime aspernatur incidunt d</p>
-        </div>
-    )
-}
-export default Hello;
