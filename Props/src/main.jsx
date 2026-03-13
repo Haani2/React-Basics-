1ebab2b80e704b72a5bf6d755e3e2d747f116cad
259833 <utkarshchaubey231@gmail.com>
2026-03-13 16:49:02 +0530

    changes save

---
+++ Props/src/main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import UserCard from './userCard'
+
+createRoot(document.getElementById('root')).render(
+
+    <UserCard name = "hani" age="22"/>
+  
+)
---
+++ Props/src/userCard.jsx
@@ -0,0 +1,10 @@
+function UserCard(props){
+    return(
+        <>
+        <h1>name:{props.name}</h1>
+        <h1>age:{props.age}</h1>
+        </>
+    );
+}
+
+export default UserCard;
